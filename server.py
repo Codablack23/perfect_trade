daa1c02493178cda3dd1c686c31adf7a3b331098
codablack <goodluckedih@yahoo.com>
2021-04-03 15:06:25 +0100

    added secret key again

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from perfect_trade import app,secret_key
 
 if __name__ == "__main__":
-    app.secret_key="qwertyuiopsdfghjkl234567890@3556787980"
+    app.secret_key="b'bV/\x1d\xc1\xf9\x96i\x15\xaa\xe9\x85A\xb0+Y\x11&\x111cz?+'"
     app.run()
      
