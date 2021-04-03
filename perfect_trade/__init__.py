b0f975169db4f473968b273f6719cce030038558
codablack <goodluckedih@yahoo.com>
2021-04-03 13:36:08 +0100

    changed email password

--- perfect_trade/__init__.py
+++ perfect_trade/__init__.py
@@ -45,7 +45,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = "goodluckedih@gmail.com"
-app.config['MAIL_PASSWORD'] = "goodybag23"
+app.config['MAIL_PASSWORD'] = "codablack23"
 mail = Mail(app)
 
 
