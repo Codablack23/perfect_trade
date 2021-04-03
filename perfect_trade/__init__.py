f6590d94dfd680588cf987992c4352ff043a400e
Codablack23 <77158925+Codablack23@users.noreply.github.com>
2021-04-03 15:47:05 +0100

    Update __init__.py

--- perfect_trade/__init__.py
+++ perfect_trade/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 # from flask_mysqldb import MySQL
 from flask_mail import Mail
-
+import os
 # class MysqlConnection(object):
 #     def __init__(self):
 #         self.host ="mysql-26183-0.cloudclusters.net"
@@ -23,7 +23,7 @@ from flask_mail import Mail
 #         connect.close()
 
 app = Flask(__name__)
-app.secret_key="b'bV/\x1d\xc1\xf9\x96i\x15\xaa\xe9\x85A\xb0+Y\x11&\x111cz?+'"
+app.secret_key=os.environ['SECRET_KEY']
 secret_key="qwertyuiopasdfghjkl11"
 host="mysql-26183-0.cloudclusters.net"
 port=26183
